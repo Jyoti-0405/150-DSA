55610f2f9ca98b56278f1b89e512660538c2a386
Jyoti-0405 <jyoti.b0405@gmail.com>
2024-05-08 18:10:48 +0530

    generate parenthesis day 14

---
+++ Stack/GenarateParenthesis.java
@@ -0,0 +1,48 @@
+package Stack;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Stack;
+// Time complexity - O(2^n * n);
+// space complexity - O(2n+2^n⋅n)
+class Solution3{
+    Stack<Character> st = new Stack<>();
+    List<String> res = new ArrayList<>();
+    public List<String> GenarateParenthesis(int n) {
+        //Intuition behind  - only add an open paranthesis if open<n
+        //only add a closing parenthrsis if close<open
+        //valid iif open == close ==n
+        backtrack(0,0,n);
+        return res;
+    }
+    private void backtrack(int openN, int closeN, int n) {
+        if(openN == closeN && closeN==n){
+            Iterator it = new st.iterator();
+            String temp = "";
+            if(it.hasNext()){
+                temp = temp+it.next();
+            }
+            res.add(temp);
+        }
+        if(openN<n){
+            st.push('(');
+            backtrack(openN+1,closeN,n);
+            st.pop();
+        }
+        if(closeN<openN){
+            st.push(')');
+            backtrack(openN,closeN+1,n);
+            st.pop();
+        }
+    }
+
+
+    }
+class Generate{
+    public static void main(String[] args) {
+        Solution3 s = new Solution3();
+        System.out.println(s.GenarateParenthesis(3));
+    }
+}
+
